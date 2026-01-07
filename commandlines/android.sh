b2eb740025e6c2dfa3cbfe9a7067852b55bd0d58
Mike DiGiovanni <vinblau@gmail.com>
2026-01-07 09:59:59 -0500

    D - Add command-line script for Android job search via LinkedIn

---
+++ commandlines/android.sh
@@ -0,0 +1,10 @@
+cd ..
+./run_local.sh \
+     --search-term "android" \
+     --location "New York" \
+     --site linkedin \
+     --results-wanted 5 \
+     --job-type fulltime \
+     --output-dir "my_jobs" \
+     --hours-old 168 \
+     --easy-apply true
