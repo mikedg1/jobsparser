a1155ad3c02c1d9e0f22f0e7b6f92203bcd18c34
Mike DiGiovanni <vinblau@gmail.com>
2026-01-07 08:05:52 -0500

    AI - Updated python build version

--- run_local.sh
+++ run_local.sh
@@ -3,4 +3,4 @@
 # Assumes this script is run from the repository root.
 
 # All arguments passed to this script will be forwarded to the jobsparser command.
-uv run --directory ./jobsparser jobsparser "$@" 
+uv run --directory ./jobsparser jobsparser "$@"
